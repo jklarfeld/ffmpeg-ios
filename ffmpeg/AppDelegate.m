1c155a2812797a02995dffbdfa8e6eec916527f1
Jeffrey Klarfeld <jklarfeld@me.com>
2014-03-06 23:41:46 -0500

    open in support for .mp4

--- ffmpeg/AppDelegate.m
+++ ffmpeg/AppDelegate.m
@@ -47,4 +47,12 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+	NSLog(@"url: %@", url);
+	NSLog(@"sourceApplication: %@", sourceApplication);
+	
+	return YES;
+}
+
 @end
